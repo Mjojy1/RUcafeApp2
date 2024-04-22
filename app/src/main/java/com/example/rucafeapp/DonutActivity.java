1608a81c131ac65b15c27f284c130bdd364a3a2e
Mjojy1 <122075382+Mjojy1@users.noreply.github.com>
2024-04-21 20:56:21 -0400

    donut

--- app/src/main/java/com/example/rucafeapp/DonutActivity.java
+++ app/src/main/java/com/example/rucafeapp/DonutActivity.java
@@ -14,8 +14,6 @@ import java.util.List;
 
 public class DonutActivity extends AppCompatActivity {
 
-   // Button backToMain;
-   // ImageView donutImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,21 +21,22 @@ public class DonutActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
-
         List<Donut> donuts = new ArrayList<Donut>();
-        donuts.add(new Donut("jelly",1.79,R.drawable.donut23));
-
-
-
-
+        donuts.add(new Donut("jelly",1.79,R.drawable.jellyd));
+        donuts.add(new Donut("vanilla Frosted",1.79,R.drawable.vanillafrosted));
+        donuts.add(new Donut("Chocolate",1.79,R.drawable.chocofrost));
+        donuts.add(new Donut("Blueberry Cake",1.79,R.drawable.blueberry));
+        donuts.add(new Donut("Double Chocolate",1.79,R.drawable.dobbychoc));
+        donuts.add(new Donut("Old Fashion",1.79,R.drawable.oldfashion));
+        donuts.add(new Donut("Glazed",1.79,R.drawable.glazed));
+        donuts.add(new Donut("Birthday Cake",1.79,R.drawable.birthcake));
+        donuts.add(new Donut("Boston Creme",1.79,R.drawable.bostoncreme));
+        donuts.add(new Donut("Strawberry Frosted",1.79,R.drawable.strawberryd));
+        donuts.add(new Donut("Munchkins",1.79,R.drawable.munchkind));
+        donuts.add(new Donut("French Crueller",1.79,R.drawable.frenchcrueller));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(),donuts));
-    //    donutImage = (ImageView) findViewById(R.id.DonutImage);
-     //   backToMain = (Button) findViewById(R.id.backToMain);
 
-    //    backToMain.setOnClickListener(v -> {
-    //        finish();
-    //    });
     }
 }
