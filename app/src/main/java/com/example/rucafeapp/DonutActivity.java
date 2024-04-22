3f22dd21d2c69665ff8073db6bad74816c81de95
Mjojy1 <122075382+Mjojy1@users.noreply.github.com>
2024-04-21 21:06:24 -0400

    donut

--- app/src/main/java/com/example/rucafeapp/DonutActivity.java
+++ app/src/main/java/com/example/rucafeapp/DonutActivity.java
@@ -23,17 +23,17 @@ public class DonutActivity extends AppCompatActivity {
 
         List<Donut> donuts = new ArrayList<Donut>();
         donuts.add(new Donut("jelly",1.79,R.drawable.jellyd));
-        donuts.add(new Donut("vanilla Frosted",1.79,R.drawable.vanillafrosted));
-        donuts.add(new Donut("Chocolate",1.79,R.drawable.chocofrost));
-        donuts.add(new Donut("Blueberry Cake",1.79,R.drawable.blueberry));
+        donuts.add(new Donut("vanilla Frosted Cake",1.89,R.drawable.vanillafrosted));
+        donuts.add(new Donut("Chocolate Cake",1.89,R.drawable.chocofrost));
+        donuts.add(new Donut("Blueberry Cake",1.89,R.drawable.blueberry));
         donuts.add(new Donut("Double Chocolate",1.79,R.drawable.dobbychoc));
         donuts.add(new Donut("Old Fashion",1.79,R.drawable.oldfashion));
-        donuts.add(new Donut("Glazed",1.79,R.drawable.glazed));
-        donuts.add(new Donut("Birthday Cake",1.79,R.drawable.birthcake));
+        donuts.add(new Donut("Glazed Cake",1.89,R.drawable.glazed));
+        donuts.add(new Donut("Plain Munchkins",.39,R.drawable.oldfashionmunch));
         donuts.add(new Donut("Boston Creme",1.79,R.drawable.bostoncreme));
         donuts.add(new Donut("Strawberry Frosted",1.79,R.drawable.strawberryd));
-        donuts.add(new Donut("Munchkins",1.79,R.drawable.munchkind));
-        donuts.add(new Donut("French Crueller",1.79,R.drawable.frenchcrueller));
+        donuts.add(new Donut("Glazed Munchkins",.39,R.drawable.glazedmunch));
+        donuts.add(new Donut("Chocolate Munchkins",.39,R.drawable.chocolate_munch));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(),donuts));
