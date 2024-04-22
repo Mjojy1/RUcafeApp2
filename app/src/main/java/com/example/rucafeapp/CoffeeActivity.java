a08ad12611d76055d03e8757cf3af35a9ae3b892
Jack Zhong <92420123+JackZ1220@users.noreply.github.com>
2024-04-22 00:17:30 -0400

    Finished Coffee and Sandwich Pages

--- app/src/main/java/com/example/rucafeapp/CoffeeActivity.java
+++ app/src/main/java/com/example/rucafeapp/CoffeeActivity.java
@@ -1,23 +1,178 @@
 package com.example.rucafeapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class CoffeeActivity extends AppCompatActivity {
 
     Button coffeeBackToMain;
+    TextInputEditText coffeeTotal;
+    Button addCoffee;
+    CheckBox irishCreamCheckBox;
+    CheckBox caramelCheckBox;
+    CheckBox sweetCreamCheckBox;
+    CheckBox mochaCheckBox;
+    CheckBox frenchVanillaCheckBox;
+    Spinner sizeSpinner;
+    Spinner quantitySpinner;
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coffee);
 
         coffeeBackToMain = (Button) findViewById(R.id.coffeeBackToMain);
+        coffeeTotal = (TextInputEditText) findViewById(R.id.coffeeTotal);
+        addCoffee = (Button) findViewById(R.id.addCoffee);
+        irishCreamCheckBox = (CheckBox) findViewById(R.id.irishCreamCheckBox);
+        caramelCheckBox = (CheckBox) findViewById(R.id.caramelCheckBox);
+        sweetCreamCheckBox = (CheckBox) findViewById(R.id.sweetCreamCheckBox);
+        mochaCheckBox = (CheckBox) findViewById(R.id.mochaCheckBox);
+        frenchVanillaCheckBox = (CheckBox) findViewById(R.id.frenchVanillaCheckBox);
+        sizeSpinner = (Spinner) findViewById(R.id.sizeSpinner);
+        quantitySpinner = (Spinner) findViewById(R.id.quantitySpinner);
+
+        ArrayList<String> sizes = new ArrayList<>();
+        sizes.add("Short");
+        sizes.add("Tall");
+        sizes.add("Grande");
+        sizes.add("Venti");
+        ArrayAdapter<String> sizeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sizes);
+        sizeSpinner.setAdapter(sizeAdapter);
+        ArrayList<String> quantities = new ArrayList<>();
+        for(int i = 1; i <= 5; i++){
+            quantities.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> quantityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, quantities);
+        quantitySpinner.setAdapter(quantityAdapter);
 
         coffeeBackToMain.setOnClickListener(v -> {
             finish();
         });
+
+        addCoffee.setOnClickListener(v -> {
+            String size = sizeSpinner.getSelectedItem().toString();
+            for(int i = 0; i < quantitySpinner.getSelectedItemPosition(); i++){
+                OrderTracker.addCoffee(size, getAddOns(), subTotal());
+            }
+        });
+
+        irishCreamCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            coffeeTotal.setText(String.format("$%.2f", subTotal()));
+        });
+        caramelCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            coffeeTotal.setText(String.format("$%.2f", subTotal()));
+        });
+        sweetCreamCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            coffeeTotal.setText(String.format("$%.2f", subTotal()));
+        });
+        mochaCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            coffeeTotal.setText(String.format("$%.2f", subTotal()));
+        });
+        frenchVanillaCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            coffeeTotal.setText(String.format("$%.2f", subTotal()));
+        });
+
+        quantitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                coffeeTotal.setText(String.format("$%.2f", subTotal()));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                coffeeTotal.setText(String.format("$%.2f", subTotal()));
+            }
+        });
+        sizeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                coffeeTotal.setText(String.format("$%.2f", subTotal()));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                coffeeTotal.setText(String.format("$%.2f", subTotal()));
+            }
+        });
+    }
+    private String getsize(){
+        return sizeSpinner.getSelectedItem().toString();
+    }
+    private double subTotal(){
+        double price = 0;
+        price += addOnPrice();
+        price += sizePrice(sizeSpinner.getSelectedItem().toString());
+        price *= Integer.parseInt(quantitySpinner.getSelectedItem().toString());
+        return price;
+    }
+    private double addOnPrice(){
+        double price = 0;
+        if(irishCreamCheckBox.isChecked()){
+            price += 0.50;
+        }
+        if(caramelCheckBox.isChecked()){
+            price += 0.50;
+        }
+        if(sweetCreamCheckBox.isChecked()){
+            price += 0.50;
+        }
+        if(mochaCheckBox.isChecked()){
+            price += 0.50;
+        }
+        if(frenchVanillaCheckBox.isChecked()){
+            price += 0.50;
+        }
+        return price;
+    }
+    private double sizePrice(String size){
+        if(size.equals("Short")){
+            return 1.99;
+        }
+        else if(size.equals("Tall")){
+            return 2.49;
+        }
+        else if(size.equals("Grande")){
+            return 2.99;
+        }
+        else{
+            return 3.49;
+        }
+    }
+    private String[] getAddOns(){
+        String[] addOns = new String[5];
+        int i = 0;
+        if(irishCreamCheckBox.isChecked()){
+            addOns[i] = "Irish Cream";
+            i++;
+        }
+        if(caramelCheckBox.isChecked()){
+            addOns[i] = "Caramel";
+            i++;
+        }
+        if(sweetCreamCheckBox.isChecked()){
+            addOns[i] = "Sweet Cream";
+            i++;
+        }
+        if(mochaCheckBox.isChecked()){
+            addOns[i] = "Mocha";
+            i++;
+        }
+        if(frenchVanillaCheckBox.isChecked()){
+            addOns[i] = "French Vanilla";
+            i++;
+        }
+        return addOns;
     }
 }
--- app/src/main/java/com/example/rucafeapp/SandwichActivity.java
+++ app/src/main/java/com/example/rucafeapp/SandwichActivity.java
@@ -1,22 +1,175 @@
 package com.example.rucafeapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SandwichActivity extends AppCompatActivity {
 
     Button backToHome;
+    TextInputEditText sandwichTotal;
+    Button addSandwich;
+    RadioButton bagelType;
+    RadioButton wheatType;
+    RadioButton sourType;
+    RadioButton beefType;
+    RadioButton chickenType;
+    RadioButton fishType;
+    RadioGroup breadGroup;
+    RadioGroup meatGroup;
+    CheckBox lettuceAddOn;
+    CheckBox tomatoAddOn;
+    CheckBox onionAddOn;
+    CheckBox cheeseAddOn;
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sandwich);
 
         backToHome = (Button) findViewById(R.id.backToHome);
+        sandwichTotal = (TextInputEditText) findViewById(R.id.sandwichTotal);
+        addSandwich = (Button) findViewById(R.id.addSandwich);
+        bagelType = (RadioButton) findViewById(R.id.bagelType);
+        wheatType = (RadioButton) findViewById(R.id.wheatType);
+        sourType = (RadioButton) findViewById(R.id.sourType);
+        beefType = (RadioButton) findViewById(R.id.beefType);
+        chickenType = (RadioButton) findViewById(R.id.chickenType);
+        fishType = (RadioButton) findViewById(R.id.fishType);
+        breadGroup = (RadioGroup) findViewById(R.id.breadGroup);
+        meatGroup = (RadioGroup) findViewById(R.id.meatGroup);
+        lettuceAddOn = (CheckBox) findViewById(R.id.lettuceAddOn);
+        tomatoAddOn = (CheckBox) findViewById(R.id.tomatoAddOn);
+        onionAddOn = (CheckBox) findViewById(R.id.onionAddOn);
+        cheeseAddOn = (CheckBox) findViewById(R.id.cheeseAddOn);
 
         backToHome.setOnClickListener(v -> {
             finish();
         });
+
+        addSandwich.setOnClickListener(v -> {
+            if(breadGroup.getCheckedRadioButtonId() == -1 || meatGroup.getCheckedRadioButtonId() == -1){
+                Toast toast = Toast.makeText(getApplicationContext(), "Please select a bread and protein", Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            double price = 0;
+            String sandwichType = "";
+            price += proteinPrice();
+
+            OrderTracker.addSandwich(getBread(), getProtein(), getAddOns(), price);
+            Toast toast = Toast.makeText(getApplicationContext(), "Sandwich added to order", Toast.LENGTH_SHORT);
+            toast.show();
+        });
+
+        meatGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            sandwichTotal.setText(String.format("%.2f", subTotal()));
+        });
+
+        lettuceAddOn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            sandwichTotal.setText(String.format("%.2f", subTotal()));
+        });
+
+        tomatoAddOn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            sandwichTotal.setText(String.format("%.2f", subTotal()));
+        });
+
+        onionAddOn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            sandwichTotal.setText(String.format("%.2f", subTotal()));
+        });
+
+        cheeseAddOn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            sandwichTotal.setText(String.format("%.2f", subTotal()));
+        });
+    }
+    private double subTotal(){
+        double price = 0;
+        price += proteinPrice();
+        price += addOnPrice();
+        return price;
+    }
+    private double addOnPrice(){
+        double price = 0;
+        if(lettuceAddOn.isChecked()){
+            price += 0.3;
+        }
+        if(tomatoAddOn.isChecked()){
+            price += 0.3;
+        }
+        if(onionAddOn.isChecked()){
+            price += 0.3;
+        }
+        if(cheeseAddOn.isChecked()){
+            price += 1.0;
+        }
+        return price;
+    }
+    private String getProtein(){
+        String protein = "";
+        if(beefType.isChecked()){
+            protein = "Beef";
+        }
+        else if(chickenType.isChecked()){
+            protein = "Chicken";
+        }
+        else if(fishType.isChecked()){
+            protein = "Fish";
+        }
+        return protein;
+    }
+    private String getBread(){
+        String bread = "";
+        if(bagelType.isChecked()){
+            bread = "Bagel";
+        }
+        else if(wheatType.isChecked()){
+            bread = "Wheat";
+        }
+        else if(sourType.isChecked()){
+            bread = "Sour";
+        }
+        return bread;
+    }
+    private double proteinPrice(){
+        double price = 0;
+        if(beefType.isChecked()){
+            price = 10.99;
+        }
+        else if(chickenType.isChecked()){
+            price = 9.99;
+        }
+        else if(fishType.isChecked()){
+            price = 8.99;
+        }
+        return price;
+    }
+    private String[] getAddOns(){
+        String[] addOns = new String[4];
+        int i = 0;
+        if(lettuceAddOn.isChecked()){
+            addOns[i] = "Lettuce";
+            i++;
+        }
+        if(tomatoAddOn.isChecked()){
+            addOns[i] = "Tomato";
+            i++;
+        }
+        if(onionAddOn.isChecked()){
+            addOns[i] = "Onion";
+            i++;
+        }
+        if(cheeseAddOn.isChecked()){
+            addOns[i] = "Cheese";
+            i++;
+        }
+        return addOns;
     }
 }
